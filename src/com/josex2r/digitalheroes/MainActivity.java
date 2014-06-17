f30ecbb83e948862dc560a7d08eec4fa54ee508f
joseX2R <jlrepresa@gmail.com>
2014-06-17 21:02:57 +0200

    Send email, loading screen NOT FINISHED

--- src/com/josex2r/digitalheroes/MainActivity.java
+++ src/com/josex2r/digitalheroes/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -43,6 +44,7 @@ public class MainActivity extends FragmentActivity {
     private List<String> fragmentTitles;
     
     //Scrollable menu
+    private int mDrawerLastPosition = 1;
 	private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
@@ -61,68 +63,72 @@ public class MainActivity extends FragmentActivity {
 		setContentView(R.layout.activity_main);
 		//ActionBar ab=getActionBar(); 
         //ab.setBackgroundDrawable( new ColorDrawable(Color.parseColor("#fefefe")) );
+		
+		Intent intent = new Intent(this, LoaderActivity.class);
+		startActivity(intent);
         
 		//Init view pager
-			mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-			mViewPager = (ViewPager) findViewById(R.id.pager);
-			mViewPager.setAdapter(mSectionsPagerAdapter);
-			mViewPager.getChildAt(0).setBackgroundResource(R.color.red);
-			//Create fragments
-			fragmentList=new ArrayList<Fragment>();
-			fragmentList.add(new CategoryPostsFragment());
-			fragmentList.add(new AllPostsFragment());
-			fragmentList.add(new AuthorPostsFragment());
-			//Create titles
-			fragmentTitles=new ArrayList<String>();
-			fragmentTitles.add(getString(R.string.title_section2));
-			fragmentTitles.add(getString(R.string.title_section1));
-			fragmentTitles.add(getString(R.string.title_section3));
-			//Add fragments and titles to the slider
-			mSectionsPagerAdapter.addFragments(fragmentList, fragmentTitles);
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+		mViewPager.getChildAt(0).setBackgroundResource(R.color.red);
+		//Create fragments
+		fragmentList=new ArrayList<Fragment>();
+		fragmentList.add(new CategoryPostsFragment());
+		fragmentList.add(new AllPostsFragment());
+		fragmentList.add(new AuthorPostsFragment());
+		//Create titles
+		fragmentTitles=new ArrayList<String>();
+		fragmentTitles.add(getString(R.string.title_section2));
+		fragmentTitles.add(getString(R.string.title_section1));
+		fragmentTitles.add(getString(R.string.title_section3));
+		//Add fragments and titles to the slider
+		mSectionsPagerAdapter.addFragments(fragmentList, fragmentTitles);
 		
 		//Init scrollable menu
-			//PagerTabStrip pts=(PagerTabStrip)findViewById(R.id.pager_title_strip);
-			
-			mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-	        mDrawerList = (ListView) findViewById(R.id.slidingMenu);
-	        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-	        
-	        List<ScrollableMenu> dataList= new ArrayList<ScrollableMenu>();
-	        dataList.add(new ScrollableMenu(getString(R.string.title_section1), getString(R.string.icon_inbox)));
-            dataList.add(new ScrollableMenu(getString(R.string.title_section2), getString(R.string.icon_bookmark)));
-            dataList.add(new ScrollableMenu(getString(R.string.title_section3), getString(R.string.icon_user)));
-            dataList.add(new ScrollableMenu(getString(R.string.title_section4), getString(R.string.icon_star)));
-	        
-	        
-	        View header=getLayoutInflater().inflate(R.layout.drawer_list_header, null);
-	        mDrawerList.addHeaderView(header, new Object(), false);
-	        mDrawerList.setAdapter(new ScrollableMenuAdapter(this, R.layout.drawer_list_item, dataList));
+		//PagerTabStrip pts=(PagerTabStrip)findViewById(R.id.pager_title_strip);
+		
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.slidingMenu);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        
+        //Scrollable menu items
+        List<ScrollableMenu> dataList= new ArrayList<ScrollableMenu>();
+        dataList.add(new ScrollableMenu(getString(R.string.title_section1), getString(R.string.icon_inbox)));
+        dataList.add(new ScrollableMenu(getString(R.string.title_section2), getString(R.string.icon_bookmark)));
+        dataList.add(new ScrollableMenu(getString(R.string.title_section3), getString(R.string.icon_user)));
+        dataList.add(new ScrollableMenu(getString(R.string.title_section4), getString(R.string.icon_star)));
+        dataList.add(new ScrollableMenu(getString(R.string.title_section5), getString(R.string.icon_mail_reply_all)));
+        
+        //Scrollable menu header
+        View header=getLayoutInflater().inflate(R.layout.drawer_list_header, null);
+        mDrawerList.addHeaderView(header, new Object(), false);
+        mDrawerList.setAdapter(new ScrollableMenuAdapter(this, R.layout.drawer_list_item, dataList));
 
-	        
-	        mDrawerList.setOnItemClickListener(new ScrollableMenuClickListener());
-	        mDrawerList.setItemChecked(1, true);
-	        // enable ActionBar app icon to behave as action to toggle nav drawer
-	        getActionBar().setDisplayHomeAsUpEnabled(true);
-	        getActionBar().setHomeButtonEnabled(true);
-	        // ActionBarDrawerToggle ties together the the proper interactions
-	        // between the sliding drawer and the action bar app icon
-	        mDrawerToggle = new ActionBarDrawerToggle(
-	                this,
-	                mDrawerLayout,
-	                R.drawable.ic_drawer,
-	                R.string.drawer_open,
-	                R.string.drawer_close
-	                ) {
-	            public void onDrawerClosed(View view) {
-	                getActionBar().setTitle(R.string.app_name);
-	                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-	            }
-	            public void onDrawerOpened(View drawerView) {
-	                getActionBar().setTitle(R.string.app_name);
-	                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-	            }
-	        };
-	        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerList.setOnItemClickListener(new ScrollableMenuClickListener());
+        mDrawerList.setItemChecked(mDrawerLastPosition, true);
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+                ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(R.string.app_name);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 		
 	}
 	
@@ -180,10 +186,9 @@ public class MainActivity extends FragmentActivity {
     
     public class ScrollableMenuClickListener implements ListView.OnItemClickListener {
     	
-
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        	
+        	Log.d("MyApp", Integer.toString(mDrawerList.getCheckedItemPosition()));
         	switch(position){
         		case 1:
         			if(blog.getActiveFilter() != Blog.FILTER_ALL){
@@ -210,14 +215,24 @@ public class MainActivity extends FragmentActivity {
         			mSectionsPagerAdapter.changeTitle(1, getString(R.string.title_section4));
         			mViewPager.setCurrentItem(1);
         			break;
+        		case 5:
+        			Intent intent = new Intent(Intent.ACTION_SEND);
+        			intent.setType("text/message");
+        			intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"codex2r@gmail.com"});
+        			intent.putExtra(Intent.EXTRA_SUBJECT, "");
+        			intent.putExtra(Intent.EXTRA_TEXT, "");
+        			Intent mailer = Intent.createChooser(intent, null);
+        			startActivity(mailer);
+        			break;
         	}
         	
-
-        	//((TextView)view.findViewById(R.id.lblItemDrawerIcon)).setTextColor(getResources().getColor(R.color.white));
-        	//((TextView)view.findViewById(R.id.lblItemDrawerText)).setTextColor(getResources().getColor(R.color.white));
-        	mDrawerList.setItemChecked(position, true);
-            //setTitle(fragmentTitles.get(position));
-            mDrawerLayout.closeDrawer(mDrawerList);
+        	if(position!=5){
+        		mDrawerList.setItemChecked(position, true);
+        		mDrawerLastPosition = position;
+        	}else{
+        		mDrawerList.setItemChecked(mDrawerLastPosition, true);
+        	}
+        	mDrawerLayout.closeDrawer(mDrawerList);
         }
     }
    
@@ -281,7 +296,7 @@ public class MainActivity extends FragmentActivity {
 
         }
         return mTrackers.get(trackerId);
-      }
+    }
 
 	public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
 
